070cd27694bb2d6d5345e6b3b8af9dcbbf446540
Leandro <leandrobcrv@gmail.com>
2023-05-17 20:01:00 -0300

    Requisito 01

---
+++ app/backend/src/database/models/teams.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const TeamTable = sequelize.define('Team', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    teamName: DataTypes.STRING
+  }, {
+    tableName: 'teams',
+    underscored: true,
+    timestamps: false,
+  })
+
+  return TeamTable;
+};
